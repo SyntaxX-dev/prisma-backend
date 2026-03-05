d077804bcf4ddddffb4cf61fb7c3e88fb2c0242f
gabrielglp <gabriel_pg08@hotmail.com>
2026-03-05 01:31:42 -0300

    chore(asaas): use only production API

--- src/infrastructure/asaas/config/asaas.config.ts
+++ src/infrastructure/asaas/config/asaas.config.ts
@@ -11,13 +11,9 @@ export default registerAs(
   'asaas',
   (): AsaasConfig => ({
     apiKey: process.env.ASAAS_API_KEY || '',
-    baseUrl:
-      process.env.ASAAS_ENVIRONMENT === 'production'
-        ? 'https://api.asaas.com/v3'
-        : 'https://api-sandbox.asaas.com/v3',
+    baseUrl: 'https://api.asaas.com/v3',
     webhookToken: process.env.ASAAS_WEBHOOK_TOKEN || '',
-    environment:
-      (process.env.ASAAS_ENVIRONMENT as 'sandbox' | 'production') || 'sandbox',
+    environment: 'production',
   }),
 );
 
